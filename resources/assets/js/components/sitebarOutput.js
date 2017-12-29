6de9164c6e85ac6b5e8315020aa578f85814ee70
lonixoon <soinov.alexey@yandex.ru>
2017-12-29 21:07:16 +0700

    Доработан выез сайдбара на странице блог. Уменьшен размен картинок для паралакса

--- resources/assets/js/components/sitebarOutput.js
+++ resources/assets/js/components/sitebarOutput.js
@@ -11,27 +11,46 @@ module.exports = (function () {
 
         let
             body = $('body'),
+            header = $('header'),
+            footer = $('footer'),
+            // боковая панель
             sidebar = $('.page-nav__wrap'),
+            // стрелка возле боковой панели
             toggle = $('.page-nav__toggle'),
             blog = $('.blog'),
+            // начало касания
             touchStartX = 0,
+            // конец касания
             touchEndX = 0,
-            threshold = 50;
+            // длинна свапа для рабатывания скрипта
+            threshold = 65;
 
-        toggle.on('click', function (event) {
-
-            event.preventDefault();
-            toggle.toggleClass('page-nav__toggle--opened');
-            sidebar.toggleClass('page-nav__wrap--opened');
-
+        // toggle.on('click', function (event) {
+        //
+        //     event.preventDefault();
+        //     toggle.toggleClass('page-nav__toggle--opened');
+        //     sidebar.toggleClass('page-nav__wrap--opened');
+        //
+        // });
+        // sidebar.on('click', function (event) {
+        //
+        //     toggle.removeClass('page-nav__toggle--opened');
+        //     sidebar.removeClass('page-nav__wrap--opened');
+        //
+        // });
+        // выез сайдбара по клику
+        $(doc).click(function (e) {
+            if (e.target !== sidebar[0] && !sidebar.has(e.target).length &&
+                e.target !== toggle[0] && !toggle.has(e.target).length) {
+                toggle.removeClass('page-nav__toggle--opened');
+                sidebar.removeClass('page-nav__wrap--opened');
+            } else if (e.target === toggle[0]) {
+                toggle.toggleClass('page-nav__toggle--opened');
+                sidebar.toggleClass('page-nav__wrap--opened');
+            }
         });
 
-        sidebar.on('click', function (event) {
 
-            toggle.removeClass('page-nav__toggle--opened');
-            sidebar.removeClass('page-nav__wrap--opened');
-
-        });
 
 
         body.on('touchstart', function (event) {
