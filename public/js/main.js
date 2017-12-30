96ba383e178a7c09e0367b960a67bee7e83c09a2
lonixoon <soinov.alexey@yandex.ru>
2017-12-30 16:09:31 +0700

    Исправления вызова сайдбара на ipad

--- public/js/main.js
+++ public/js/main.js
@@ -577,7 +577,7 @@ module.exports = (function () {
 
         // выез сайдбара по клику:
         // вещаем обработчик кликов на страницу
-        $(doc).click(function (e) {
+        $(doc).on('click', function (e) {
             // если клик произошол вне наших элементов или их потомков
             if (e.target !== sidebar[0] && !sidebar.has(e.target).length &&
                 e.target !== toggle[0] && !toggle.has(e.target).length) {
