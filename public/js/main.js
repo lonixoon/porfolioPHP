c287e1a00ef765c88ae5b7f030f8c93f53784fb8
lonixoon <soinov.alexey@yandex.ru>
2017-12-30 10:26:25 +0700

    Сделал стрелку в сайтбаре на странице блок мобильная версия

--- public/js/main.js
+++ public/js/main.js
@@ -574,38 +574,43 @@ module.exports = (function () {
         //     sidebar.removeClass('page-nav__wrap--opened');
         //
         // });
-        // выез сайдбара по клику
+
+        // выез сайдбара по клику:
+        // вещаем обработчик кликов на страницу
         $(doc).click(function (e) {
+            // если клик произошол вне наших элементов или их потомков
             if (e.target !== sidebar[0] && !sidebar.has(e.target).length &&
                 e.target !== toggle[0] && !toggle.has(e.target).length) {
+                // удаляем класс
                 toggle.removeClass('page-nav__toggle--opened');
                 sidebar.removeClass('page-nav__wrap--opened');
+                // если клик произошёл по стрекле
             } else if (e.target === toggle[0]) {
+                // меняем класс
                 toggle.toggleClass('page-nav__toggle--opened');
                 sidebar.toggleClass('page-nav__wrap--opened');
             }
         });
 
-
-
-
+        // отслеживаем начало касания
         body.on('touchstart', function (event) {
-
             let touch = event.originalEvent.touches[0] || event.originalEvent.changedTouches[0];
             touchStartX = touch.pageX;
-
         });
 
-
+        // отслеживаем конец касания
         body.on('touchend', function (event) {
-
             let touch = event.originalEvent.touches[0] || event.originalEvent.changedTouches[0];
             touchEndX = touch.pageX;
 
+            // если разность от конца до начала касание больше значения threshold
             if (touchEndX - touchStartX > threshold) {
+                // добавляем класс
                 sidebar.addClass('page-nav__wrap--opened');
                 toggle.addClass('page-nav__toggle--opened');
+                // если разность от конца до начала касание меньше отрицательного значения threshold
             } else if (touchEndX - touchStartX < -threshold) {
+                // убераем класс
                 sidebar.removeClass('page-nav__wrap--opened');
                 toggle.removeClass('page-nav__toggle--opened');
             }
